52edb22bd92de3c831b60793c901d09fc4df6a5b
Ste-cracco <s.cracco@yahoo.it>
2022-10-10 16:50:28 +0200

    CSS pagina singola creato

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -4,11 +4,37 @@
 
 @section('content')
 
-<div class="container">
-    <img src="{{$comic['thumb']}}" alt="">
-    <h2>{{$comic['title']}}</h2>
-    <p>{{$comic['description']}}</p>
-    <span> {{$comic['price']}} </span>
-</div>
+<main class="dettaglio_comic">
+    <div class="jumbotron"> </div>
+    <div class="container_comic">
+        <div class="text">
+            <h2>{{$comic['title']}}</h2>
+            <div class="prezzo">
+                <span> US Price: {{$comic['price']}} </span>
+                <span>AVAIABLE</span>
+            </div>
+            <p>{{$comic['description']}}</p>
+        </div>
+        <div class="cover">
+            <img src="{{$comic['thumb']}}" alt="">
+        </div>        
+    </div>
+    <section class="maggiori_info">
+        <div class="autori">
+            <div class="art_by">
+                <h5>Art By:</h5>
+                @foreach ($comic['artists'] as $item)
+                    <span> {{ $item }},  </span>                    
+                @endforeach
+            </div>
+            <div class="written_by">
+                <h5>Written By:</h5>
+                @foreach ($comic['writers'] as $item)
+                    <span> {{ $item }}, </span>                    
+                @endforeach
+            </div>
+        </div>
+    </section>
+</main>
 
 @endsection
